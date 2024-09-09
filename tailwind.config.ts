96542250a82504669a401443b987c8adbcbb4307
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-09-08 21:59:19 -0500

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+        black: {
+          DEFAULT: '#000',
+          100: '#000319'
+        }
+      }
+
     },
   },
   plugins: [],
