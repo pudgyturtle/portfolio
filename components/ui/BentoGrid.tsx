2374c4979809eb49b0cac51d7a8e269c5144ce9e
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-09-12 10:19:49 -0500

    Added 3d pin cards for project section

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -68,7 +68,7 @@ export const BentoGridItem = ({
         backgroundColor: 'linear-gradient(90deg, rgba(17,17,23,1) 0%, rgba(0,115,115,1) 100%)',
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full`}>
+      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
           <div className="w-full h-full absolute">
             {img && (
               <img 
@@ -89,7 +89,7 @@ export const BentoGridItem = ({
       </div>
       {id === 6 && (
         <BackgroundGradientAnimation>
-          <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+          {/*<div className="absolute z-50 flex items-center justify-center text-white font-bold"/> */}
         </BackgroundGradientAnimation>
       )}
 
@@ -97,7 +97,7 @@ export const BentoGridItem = ({
         titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
       )}
       >
-        <div className="font-sans font-extralight text-[#c1c2d3] text-neutral-600 text-sm md: text-xs lg:text-base z-10">
+        <div className="font-sans font-extralight text-[#c1c2d3] text-sm md: text-xs lg:text-base z-10">
           {description}
         </div>
         <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
@@ -110,16 +110,16 @@ export const BentoGridItem = ({
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
               <div className="flex flex-col gap-3 lg:gap-8 ">
                 {['React.js', 'Next.js', 'Tailwind CSS'].map((item) => (
-                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#172424]">
+                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#292929]">
                     {item}
                   </span>
                 ))}
-                <span className="py-4 px-3 rounded-lg text-center bg-[#172424]" />
+                <span className="py-4 px-3 rounded-lg text-center bg-[#292929]" />
               </div>
               <div className="flex flex-col gap-3 lg:gap-8 ">
-              <span className="py-4 px-3 rounded-lg text-center bg-[#172424]" />
+              <span className="py-4 px-3 rounded-lg text-center bg-[#292929]" />
                 {['MongoDB', 'PostgreSQL', 'Python'].map((item) => (
-                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#172424]">
+                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#292929]">
                     {item}
                   </span>
                 ))} 
@@ -128,7 +128,7 @@ export const BentoGridItem = ({
           )}
           {id === 6 && (
             <div className="mt-5 relative">
-              <div className={`absolute  -bottom-5 right-0`}
+              <div className={`absolute -bottom-5 right-0`}
               >
                 <Lottie options={{
                   loop: copied,
@@ -144,7 +144,7 @@ export const BentoGridItem = ({
                   title={copied ? 'Email copied' : 'Copy my email'}
                   icon={<IoCopyOutline />}
                   position="left"
-                  otherClasses="!bg-[#161A31]"
+                  otherClasses="!bg-[#111117]"
 
                   handleClick={handleCopy}
                />
