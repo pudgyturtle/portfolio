c91483f231cf81b4469df4040160d12b7e6adb6b
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-09-12 11:58:18 -0500

    Fixed hydration error

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -12,8 +12,24 @@ const RecentProjects = () => {
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
             <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
-                <PinContainer>
-                    {title}
+                <PinContainer title={title} href={link}>
+                    <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                        <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                            <img src="/bg.png" alt="bg-img" />
+                        </div>
+                        <img 
+                            src={img}
+                            alt={title}
+                            className="z-10 absolute bottom-0"
+                        />
+                    </div>
+                    <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                        {title}
+                    </h1>
+
+                    <p className="lg:text-xl lg:font-normal font-light text-sm">
+                        {des}
+                    </p>
                 </PinContainer>
             </div>
         ))}
--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -49,7 +49,7 @@ export const PinContainer = ({
           style={{
             transform: transform,
           }}
-          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
+          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
         >
           <div className={cn(" relative z-50 ", className)}>{children}</div>
         </div>
@@ -67,10 +67,10 @@ export const PinPerspective = ({
   href?: string;
 }) => {
   return (
-    <motion.div className="pointer-events-none  w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
+    <motion.div className="pointer-events-none  w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
+          <div
             href={href}
             target={"_blank"}
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
@@ -80,7 +80,7 @@ export const PinPerspective = ({
             </span>
 
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
+          </div>
         </div>
 
         <div
