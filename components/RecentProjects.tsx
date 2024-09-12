1d835e406f53d1f5fd48b69b92a2ea73491f0f7a
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-09-12 12:11:26 -0500

    Added icons to project cards

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -30,6 +30,20 @@ const RecentProjects = () => {
                     <p className="lg:text-xl lg:font-normal font-light text-sm">
                         {des}
                     </p>
+
+                    <div className="flex items-center justify-between mt-7 mb-3">
+                        <div className="flex item-center">
+                            {iconLists.map((icon, index) => (
+                                <div key={icon} className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center" 
+                                    style={{
+                                        transform: `translateX(-${5 * index * 2}px)`
+                                    }}>
+                                    <img src={icon} alt={icon} className="p-2"/>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
                 </PinContainer>
             </div>
         ))}
