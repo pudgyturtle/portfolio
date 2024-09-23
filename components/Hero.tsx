9f16ee09c1a69577af345fed9d182c8f896647fb
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-09-23 13:10:11 -0500

    Added Sentry user feedback widget

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
         <div className="flex justify-center relative my-20 z-10 ">
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-85">
-                    Modern Full Stack Web Development
+                    Modern Full Stack Software Development
                 </h2>
 
                 <TextGenerateEffect 
