da6f526cd99a9245496a0f49e4e16e0866e70dd3
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-09-25 11:26:31 -0500

    Added work experience section

---
+++ components/Experience.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button } from './ui/MovingBorders'
+import { workExperience } from '@/data'
+
+const Experience = () => {
+  return (
+    <div className="py-20" id="testimonials">
+      <h1 className="heading">
+        My 
+        <span className="text-green"> work experience</span>
+      </h1>
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
+        {workExperience.map((card => (
+            // eslint-disable-next-line react/jsx-key
+            <Button>
+               <div>
+                <img src={card.thumbnail} alt={card.thumbnail} className="lg:w-32 md:w-20 w-16"  />
+               </div> 
+            </Button>
+        )))}
+      </div >
+    </div>
+  )
+}
+
+export default Experience
